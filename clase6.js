b786328f6d88e93e1fecc04c621b952a2addde07
Lucas Garcia <lucas.garcia1202@icloud.com>
2022-08-03 11:41:54 -0300

    Nuevos apuntes

--- clase6.js
+++ clase6.js
@@ -42,3 +42,53 @@ const numeros =[1, 2, 3, 4, 5];
 for (let index = 0; index < 5; index++) {
     alert(numeros[index]);
 }
+
+//ARRAY: METOFOS Y PROPIEDADES
+//LENGTH
+//Al igual que en un string, la propiedad length nos sirve para obtenes el largo de un ARRAY.
+//Nos permite identificar cuantos elementos tiene.
+
+const miArray = ["marca", 3, "palabra"];
+console.log(miArray.length); //impreme 3
+
+//LENGTH es muy comun utilizarlo para definir el limite de una iteracion sobre un array
+
+let numerosA = [1, 2, 3, 4, 5, 6, 7, 8];
+for (let i = 0; i < numerosA.length; i++){
+    alert(numerosA[i]);
+}
+
+
+//AGRERAR ELEMENTOS
+//Para sumar un elementos a un ARRAY ya existente, se utiliza el metodo push, pasando como parametro el valor o variable a agregar
+//Siempre lo agrega al final del array
+miArray.push('otro elemento');
+
+console.log(miArray.length) //imprime 4
+console.log(miArray)
+//["marca", 3, "palabra", "otro elemento"]; AGREGA SIEMPRE AL FINAL DEL ARRAY
+
+//METODO UNSHIFT()
+//Si queremos agregar al inicio del array, utilizamos el metodo unshift()
+
+miArray.unshift('otro elemento');
+
+console.log(miArray);
+//["otro elemento", "marca", 3, "palabra", "otro elemento"]; //Agrega al inicio del array
+
+
+//QUITAR ELEMENTOS
+//Si queremos eliminar el primer elemento del array utilizamos el metodo SHIFT()
+//Para elimimar el ultimo, el metodo POP()
+
+
+//SPLICE
+//El metodo SPLICE() eliminar uno o varios elementos de un array en cualquier posicion
+//Funciona con 2 parametros: el primero es el indice donde se ubica el metodo para trabajar.
+//El segundo es la cantidad de elementos a eliminar desde esa posicion
+
+const nombres = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanesa'];
+nombres.splice(1, 2) //Pedro, miguel
+
+console.log(nombres)
+//Quedaron ['Rita', 'Ana', 'Vanesa']
